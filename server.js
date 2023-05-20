3668259ca70eeb17cc02f361125bd84b0fe8a31e
BroGrammer137 <jua16006@byui.edu>
2023-05-20 17:36:28 -0600

    Updated Code

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const Redis = require('redis');
+const { createHash } = require("node:crypto");
+
 const app = express();
+
 const port = 3000;
-const reddisClient = Redis.createClient();
 
 
-app.use(bodyParser.json()); 
+const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
+app.use(bodyParser.json());
+
 
 app.listen(port, ()=>{
 
-    reddisClient.connect();
+    redisClient.connect();
     console.log("Listening on port:"+ port);
-})
+});
 
 
 app.get('/',(req,res)=>{
@@ -24,15 +25,26 @@ app.get('/',(req,res)=>{
 
 
 
-app.post('/login',(req,res)=>{
+app.post('/login',async (req,res)=>{
     const loginBody = req.body;
     const userName = loginBody.userName;
-    if (password==="Supersafe123!"){
+    const password = loginBody.password; // we need to hash
+    const hashedPassword = password==null? null : createHash('sha3-256').update(password).digest('hex');
+    console.log("Hashed Password: "+hashedPassword);
+    const redisPassword = password==null ? null : await redisClient.hGet('hashedpasswords',userName);
+    // const hashedpassword = createHash('sha3-256').update(password).digest('hex');
+    // console.log("Hashed password: "+hashedpassword);
+    // const redisPassword = await redisClient.hGet('hashedpassword',userName);
+    console.log("Redis password for: "+ userName+": "+redisPassword);
+    if (hashedPassword===redisPassword){
         res.send("Welcome"+userName);
     } else {
         res.status(401);
-        res.send("Incorrect password")
+        res.send("Incorrect password");
     }
         
 
-})
+});
+
+
+
